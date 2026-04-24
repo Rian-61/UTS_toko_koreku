4c5523ccb8de50f760e135a23881ee16d4054db6
Rian-61 <rian80344@gmail.com>
2026-04-24 13:39:14 +0700

    feat: tambah ApiConstants
    berisi base url, endpoint, dan timeout API

---
+++ lib/core/constans/api_constans.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'http://192.168.100.2:8081/v1';
+  static const String verifyToken = '/auth/verify-token';
+  static const String products = '/products';
+  static const int connectTimeout = 30000;
+  static const int receiveTimeout = 30000;
+}
