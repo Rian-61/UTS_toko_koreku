1da8b1cdf9a1f1dfeb6cf542bfc7c255398888f0
Rian-61 <rian80344@gmail.com>
2026-04-24 15:25:38 +0700

    feat: mengimplementasikan AuthRepositoryImpl
    menghubungkan verifikasi token Firebase ke backend menggunakan Dio
    
    menyimpan access token ke secure storage dan menambahkan fungsi logout

---
+++ lib/features/auth/domain/repositories/auth_repository_impl.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import '../../../../core/constants/api_constants.dart';
+import '../../../../core/services/dio_client.dart';
+import '../../../../core/services/secure_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import '../../data/models/auth_response_model.dart';
+import 'auth_repository.dart';
+ 
+class AuthRepositoryImpl implements AuthRepository {
+  final _dio = DioClient.instance;
+  final _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
+
+  @override
+  Future<AuthResponseModel> verifyFirebaseToken(String firebaseToken) async {
+    final response = await _dio.post(
+      ApiConstants.verifyToken,
+      data: {'firebase_token': firebaseToken},
+    );
+    final authData = AuthResponseModel.fromJson(response.data['data']);
+    await SecureStorage.saveToken(authData.accessToken);
+    return authData;
+  }
+ 
+  @override
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+    await _googleSignIn.signOut();
+    await SecureStorage.deleteToken();
+  }
+}
