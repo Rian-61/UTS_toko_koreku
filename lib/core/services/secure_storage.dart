1e2108f9196f2740d47e6ec6504bc3ac58727039
Rian-61 <rian80344@gmail.com>
2026-04-24 13:50:05 +0700

    feat: menambahkan secure storage untuk token
    mengimplementasikan penyimpanan JWT menggunakan flutter_secure_storage
    
    menambahkan fungsi save, get, delete, dan pengecekan token

---
+++ lib/core/services/secure_storage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+ 
+class SecureStorage {
+  static const _storage = FlutterSecureStorage();
+  static const _keyToken = 'backend_jwt_token';
+ 
+  static Future<void> saveToken(String token) async {
+    await _storage.write(key: _keyToken, value: token);
+  }
+ 
+  static Future<String?> getToken() async {
+    return await _storage.read(key: _keyToken);
+  }
+ 
+  static Future<void> deleteToken() async {
+    await _storage.delete(key: _keyToken);
+  }
+ 
+  static Future<bool> hasToken() async {
+    final token = await _storage.read(key: _keyToken);
+    return token != null && token.isNotEmpty;
+  }
+}
