57e2758dfeb2a472fa7e7c5dded8ae645e181f09
Rian-61 <rian80344@gmail.com>
2026-04-24 13:47:56 +0700

    chore: menambahkan konfigurasi tema aplikasi
    membuat AppTheme untuk pengaturan tampilan global
    
    mengatur warna, font, AppBar, button, dan input field

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../constants/app_colors.dart';
+ 
+class AppTheme {
+  static ThemeData get lightTheme => ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary,
+      primary: AppColors.primary,
+      error: AppColors.error,
+    ),
+    fontFamily: 'Roboto',
+    scaffoldBackgroundColor: AppColors.background,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.primary,
+      foregroundColor: Colors.white,
+      elevation: 0,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        foregroundColor: Colors.white,
+        minimumSize: const Size(double.infinity, 52),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+    ),
+  );
+}
